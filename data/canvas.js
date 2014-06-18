8e98fb2b1b5cec140b4c9550d1c67ed009a0c233
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-06-19 00:00:12 +0900

    Add files

---
+++ data/canvas.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d'),
+    ctxInfo = {
+      1: [18, 2, 17],
+      2: [11, 0, 15],
+      3: [7, 1, 13],
+      4: [6, 0, 13]
+    };
+
+addon.on('message', ({count, duplicated}) => {
+  var [size, xPos, yPos] = ctxInfo[count.length];
+
+  ctx.font = size + 'pt Arial';
+  ctx.fillStyle = duplicated ? 'red' : 'black';
+  ctx.fillText(count, xPos, yPos);
+
+  addon.postMessage(canvas.toDataURL());
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
