81bb55823470fa4b9f74ad8dbebe849e8b9ed080
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-01-20 13:51:46 +0900

    Update codes for Firefox 35+

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Hatena Bookmark Counter
 =======================
 
-simple Hatena Bookmark Counter for Firefox 29+ by Add-on SDK
+simple Hatena Bookmark Counter for Firefox 35+ by Add-on SDK
 
 **This extention is experimental. You shouldn't use regularly this.**
--- data/canvas.js
+++ data/canvas.js
@@ -1,16 +1,17 @@
 'use strict';
 
-var canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d'),
-    ctxInfo = {
-      1: [18, 2, 17],
-      2: [11, 0, 15],
-      3: [7, 1, 13],
-      4: [6, 0, 13]
-    };
+const ctxInfo = {
+  1: [18, 2, 17],
+  2: [11, 0, 15],
+  3: [7, 1, 13],
+  4: [6, 0, 13]
+};
+
+let canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
 
 self.on('message', ({count, duplicated}) => {
-  var [size, xPos, yPos] = ctxInfo[count.length];
+  let [size, xPos, yPos] = ctxInfo[count.length];
 
   ctx.font = size + 'pt Arial';
   ctx.fillStyle = duplicated ? 'red' : 'black';
